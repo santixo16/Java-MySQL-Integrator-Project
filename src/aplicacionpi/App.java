daf97923faaebe752af5b893f7cbd57fbb26ee1f
Santiago Hernandez <santixo2022@gmail.com>
2023-04-07 17:57:51 -0500

    updating files

---
+++ src/aplicacionpi/App.java
@@ -0,0 +1,15 @@
+package aplicacionpi;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Bienvenid@");
+      
+        ConnectionDB conexion = new ConnectionDB("techcare");
+
+        conexion.conectar();
+        conexion.desconectar();
+
+    }
+}
+ 
